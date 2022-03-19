cbadbc4dec8b6179650ee6704c15f07274a9f23e
shemha <yonagumori314@yahoo.co.jp>
2022-03-19 17:45:57 +0900

    fix flash message

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -12,6 +12,8 @@
     @component('components.header')
     @endcomponent
     <div class="container pt-5 pb-5">
+        @component('components.flash')
+        @endcomponent
         @yield('content')
     </div>
 
